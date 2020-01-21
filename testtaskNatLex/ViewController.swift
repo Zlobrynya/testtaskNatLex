4f2fc5a4e96d53d9de73e5e2da7e3018f4efb036
Zlobrynya <zlobrynya@gmail.com>
2020-01-21 12:48:04 +0300

    First Commit
    Init commit

---
+++ testtaskNatLex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testtaskNatLex
+//
+//  Created by Nikitin Nikita on 21/01/2020.
+//  Copyright © 2020 Zlobrynya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
